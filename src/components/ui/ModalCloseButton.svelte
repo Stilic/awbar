22530faa5be3e618a028b22278ff58e90b1f94d3
Stilic <lateman-jpeg@outlook.fr>
2023-05-10 14:51:48 +0200

    chore: change icons to heroicons

--- src/components/ui/ModalCloseButton.svelte
+++ src/components/ui/ModalCloseButton.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import {UilMultiply} from 'svelte-unicons';
+  import {XMarkIcon} from '@krowten/svelte-heroicons';
 
   export let onClose: () => void;
 </script>
 
-<button class="relative right-1 top-2 float-right" on:click={onClose}><UilMultiply /></button>
+<button class="relative right-1 top-2 float-right" on:click={onClose}
+  ><XMarkIcon class="h-5 w-5" solid /></button>
